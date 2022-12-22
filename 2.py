6b44caaca2642ee9fbc464de858dad070b16b9fe
RahulKaler <121226455+RahulKaler@users.noreply.github.com>
2022-12-22 17:16:36 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,5 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> inc=int(input("enter income:"))
+... num_of_dep=int(input("enter a number of dependants:"))
+... taxic=(inc-10000)-(num_of_dep*3000)
